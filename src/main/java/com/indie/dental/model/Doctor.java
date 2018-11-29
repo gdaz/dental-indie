556ab014e2856ebb002ab9f14dc4560d389a9270
sathaphorn <clicktamproject@gmail.com>
2018-11-30 01:54:43 +0700

    Test Jenkins 2

---
+++ src/main/java/com/indie/dental/model/Doctor.java
@@ -0,0 +1,53 @@
+package com.indie.dental.model;
+
+import java.io.Serializable;
+import java.math.BigInteger;
+
+public class DoctorBean implements Serializable {
+
+    private BigInteger id;
+    private String firstName;
+    private String lastName;
+    private String age;
+    private BigInteger version;
+
+    public BigInteger getId() {
+        return id;
+    }
+
+    public void setId(BigInteger id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public BigInteger getVersion() {
+        return version;
+    }
+
+    public void setVersion(BigInteger version) {
+        this.version = version;
+    }
+}
